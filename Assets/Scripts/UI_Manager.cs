e1e6506797d55b9ec753dd1cf24467c15e277a35
emeldi <78385236+emeldi@users.noreply.github.com>
2021-07-10 21:31:56 -0700

    Set up animation system for button control

---
+++ Assets/Scripts/UI_Manager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Manager : MonoBehaviour
+{
+    
+    public void Next()
+    {
+
+    }
+
+    public void Previous()
+    {
+
+    }
+}
